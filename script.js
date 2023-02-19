a14ee3d981a253997a1da76c0e5c74e93616ada8
tgorsk15 <tgbusiness463@gmail.com>
2023-02-19 10:05:28 -0600

    Add to for loop and modify functions
    Trying to add to my loop and have 5 rounds be played. Meanwhile, I need to call upon the playerSelection and computerSelection 5 different times to
    get 5 different answers for both

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ function getComputerChoice() {
     return gameOutcomes[Math.floor(Math.random() * gameOutcomes.length)];
 }
 
+// trying to write a separate function whose sole purpose is to get the player's selection
+function getPlayerSelection() {
+    return prompt('Do you want to choose rock, paper,or scissors?', '').toLowerCase();
+}
+
+const playerSelection = getPlayerSelection()
 const computerSelection = getComputerChoice()
-const playerSelection = prompt('Do you want to choose rock, paper,or scissors?', '').toLowerCase();
+// setting up two different variables that keep track of each player's score
 let playerScore = 0
 let computerScore = 0
         // function, playRound(), that plays a SINGLE ROUND of rock/p/s
@@ -15,6 +21,8 @@ let computerScore = 0
             // return string that says who won (ex: 'Computer wins! Paper beats rock!')
             // *****PROBLEM: it is fading out my 'computerSelection' parameter, which was defined above
 function playRound(playerSelection, computerSelection) {
+    let playerSelection = prompt('Do you want to choose rock, paper,or scissors?', '').toLowerCase();
+    let computerSelection = getComputerChoice()
     // make their choice case insenstive
     // make an else option at the end if user didn't put in any of the three possible answers
     // IMPORTANT: changed all of the '=s' to '===' and it started correctly using the logic of the selections and round outcomes
@@ -22,8 +30,10 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === 'rock') {
             alert("It's a tie! You both picked Rock")
         } else if (computerSelection === 'paper') {
+            computerScore ++
             alert("You lost! Paper beats Rock")
         } else {
+            playerScore ++
             alert("You won! Rock beats Scissor!")
         }
     } else if (playerSelection === 'paper') {
@@ -52,13 +62,14 @@ function playRound(playerSelection, computerSelection) {
             // use loops!
             // use console.log() to display the results of each round AND the winner at the end
 function game() {
-    // setting up two different variables that keep track of each player's score
-    playRound(playerSelection, computerSelection)
     // create a loop... that ends after 5 rounds
     // IDEA: Only increment i IF either playerScore or computerScore increased... otherwise invoke playRound() again without adding an instance
     // Maybe not... its only asking for 5 rounds
+    // IDEA: use an if statement to check what the score is if i < 5, otherwise if i = 5, give the resultof the game
     for (let i = 0; i < 5; i++) {
-        
+        getComputerChoice()
+        playerSelection()
+        playRound(playerSelection, computerSelection)
     }
 
 }
