4020de406d7aba8148ef7a0cce1c57145e179094
tgorsk15 <tgbusiness463@gmail.com>
2023-02-22 20:17:23 -0600

    REvise and reset JS file
    After trying to put together the game() function with the for loop that would trigger 5
    rounds, my code went off the rails and when I tried to comment out the new function,
    the basic logic of the game would no longer work. So, i had to revise and change the code to
    get the nasic logic of playRound() to work again. As of this commit, I can play one round
    of the game and the logic will work. I TOOK OUT the parameters of the playRound funtion:
    playRound(playerSelection, computerSelection) --> playRound()

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log('Hya partner')
 // create a function (getComputerChoice) that randomly generates rock, paper or scissors
         // Note: use the console to mak sure this function is working correctly
 const gameOutcomes = ['rock', 'scissors', 'paper'];
@@ -6,13 +5,12 @@ function getComputerChoice() {
     return gameOutcomes[Math.floor(Math.random() * gameOutcomes.length)];
 }
 
-// trying to write a separate function whose sole purpose is to get the player's selection
-function getPlayerSelection() {
-    return prompt('Do you want to choose rock, paper,or scissors?', '').toLowerCase();
-}
+// trying to write a separate function whose sole purpose is to get the player's selection .... not working, deleted
 
-const playerSelection = getPlayerSelection()
+
+const playerSelection = prompt('Do you want to choose rock, paper,or scissors?', '').toLowerCase();
 const computerSelection = getComputerChoice()
+
 // setting up two different variables that keep track of each player's score
 let playerScore = 0
 let computerScore = 0
@@ -21,19 +19,16 @@ let computerScore = 0
             // return string that says who won (ex: 'Computer wins! Paper beats rock!')
             // *****PROBLEM: it is fading out my 'computerSelection' parameter, which was defined above
 function playRound(playerSelection, computerSelection) {
-    let playerSelection = prompt('Do you want to choose rock, paper,or scissors?', '').toLowerCase();
-    let computerSelection = getComputerChoice()
+    
     // make their choice case insenstive
     // make an else option at the end if user didn't put in any of the three possible answers
     // IMPORTANT: changed all of the '=s' to '===' and it started correctly using the logic of the selections and round outcomes
     if (playerSelection === 'rock') {
         if (computerSelection === 'rock') {
             alert("It's a tie! You both picked Rock")
-        } else if (computerSelection === 'paper') {
-            computerScore ++
+        } else if (rand === 'paper') {
             alert("You lost! Paper beats Rock")
         } else {
-            playerScore ++
             alert("You won! Rock beats Scissor!")
         }
     } else if (playerSelection === 'paper') {
@@ -58,20 +53,20 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
+
+playRound()
+
         // write another function game(), that calls the playRound() function INSIDE of this that plays a 5 round game and keeps score and reports winner at the end
             // use loops!
             // use console.log() to display the results of each round AND the winner at the end
-function game() {
+// function game() {
     // create a loop... that ends after 5 rounds
     // IDEA: Only increment i IF either playerScore or computerScore increased... otherwise invoke playRound() again without adding an instance
     // Maybe not... its only asking for 5 rounds
     // IDEA: use an if statement to check what the score is if i < 5, otherwise if i = 5, give the resultof the game
-    for (let i = 0; i < 5; i++) {
-        getComputerChoice()
-        playerSelection()
-        playRound(playerSelection, computerSelection)
-    }
+  //  for (let i = 0; i < 5; i++) {
+//    }
 
-}
+// }
 
-game()
+// game()
